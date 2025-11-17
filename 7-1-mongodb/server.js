986810cd8b77da1602c1e77e28987267d2c33a74
Vuir <Taevio@hotmail.com>
2025-11-17 11:53:25 +0300

    Todo4

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -191,7 +191,11 @@ async function createStudents() {
       createStudents();
 
 // read document
-
+async function readStudents() {
+         const all = await Student.find();
+         console.log(all);
+      }
+      readStudents();
 
 // update document
 
