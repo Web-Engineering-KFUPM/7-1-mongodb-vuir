3d976b8d00a1db26343912090c5a8d50907c3b5a
Vuir <Taevio@hotmail.com>
2025-11-17 12:02:00 +0300

    Todo5

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -198,10 +198,18 @@ async function readStudents() {
       readStudents();
 
 // update document
+async function updateStudent() {
+         await Student.updateOne({ name: "Ali" }, { age: 22 });
+         console.log("✅ Updated Ali");
+      }
 
-
+      updateStudent();
 // delete document
-
+async function deleteStudent() {
+         await Student.deleteOne({ name: "Sara" });
+      console.log("✅ Deleted Sara");
+      }
+      deleteStudent();
 
 
 
